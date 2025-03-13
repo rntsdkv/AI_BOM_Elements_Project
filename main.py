c7b6065474adc3414b9793dc4cd0cd7c39cfc994
Садыков Ренат Маратович <remasadykov@edu.hse.ru>
2025-03-13 16:18:59 +0300

    add main page

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# скоро тут будет код
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=3550)
