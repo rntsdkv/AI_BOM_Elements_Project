3211f1959162294c3150d5af6ec118bd117a00c5
Садыков Ренат Маратович <remasadykov@edu.hse.ru>
2025-03-20 14:02:48 +0300

    add functions to get datasheets and save files

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/111.0.0.0 Safari/537.36'}
@@ -45,6 +46,44 @@ def get_chipdip(query):
 # print(get_chipdip("Магниторезистивный датчик цифровой"))
 
 
+def get_datasheet(url):
+    '''
+    Позволяет получить ссылку для загрузки даташита элемента
+    по ссылке на страницу элемента
+    :param url: ссылка на страницу элемента
+    :return: список с ссылками на файлы даташитов
+    '''
+    response = requests.get(url, headers=headers)
+    parser = BeautifulSoup(response.content, "html.parser")
+    datasheets = parser.find_all('a', attrs={'class': 'link download__link with-pdfpreview'})
+    datasheet_urls = [sheet.get('href') for sheet in datasheets]
+    return datasheet_urls
+
+# print(get_datasheet("https://www.chipdip.ru/product/ld7522ps"))
+
+
+def download_file(url, name=None):
+    '''
+    Позволяет сохранить файл на сервер
+    :param url: ссылка на файл
+    :param name: (опционально) задать имя файла в системе
+    :return: True, если сохранение успешно, False – в ином случае
+    '''
+    response = requests.get(url)
+    if name:
+        file_path = name
+    else:
+        file_path = url.split('/')[-1]
+
+    if response.status_code == 200:
+        with open(os.path.join('downloads', file_path), 'wb') as file:
+            file.write(response.content)
+        return True
+    return False
+
+# print(download_file("https://static.chipdip.ru/lib/046/DOC003046561.pdf"))
+
+
 def parse_bom(file: str):
     if file.endswith('.xlsx'):
         data = pd.read_excel(file, index_col='Index')
@@ -57,4 +96,4 @@ def parse_bom(file: str):
         return data
 
 
-print(parse_bom('bom_examples/bom_example.xlsx'))
+# print(parse_bom('bom_examples/bom_example.xlsx'))
